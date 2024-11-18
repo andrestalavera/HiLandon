102b1ae7cbd4b24cfcddbe221cb7269a92c13e2c
Andres Talavera <andres.talavera@live.fr>
2024-11-18 10:01:05 +0100

    Upgrade to .NET 9

--- HiLandon.Infrastructure/ApplicationDbContext.cs
+++ HiLandon.Infrastructure/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using HiLandon.Core.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 
 namespace HiLandon.Infrastructure;
 
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext
 {
     public DbSet<Post>? Posts { get; set; }
 
@@ -14,4 +16,24 @@ public class ApplicationDbContext : DbContext
 #if V2||V3
     public DbSet<Tag>? Tags { get; set; }
 #endif
+}
+
+
+/// <summary>Class used by EF Core tools to create the DbContext.</summary>
+public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+	/// <summary>Creates the DbContext.</summary>
+	public ApplicationDbContext CreateDbContext(string[] args)
+	{
+		// For local development: get the connection string from the user secrets of the backend project
+		IConfigurationRoot configuration = new ConfigurationBuilder().Build();
+		string? connectionString = configuration.GetConnectionString("DefaultConnection");
+
+		if (connectionString == null)
+			throw new Exception("No connection string found.");
+
+		DbContextOptionsBuilder<ApplicationDbContext> builder = new();
+		DbContextOptions<ApplicationDbContext> options = builder.UseSqlServer(connectionString, x => x.CommandTimeout(900)).Options;
+		return new(options);
+	}
 }
